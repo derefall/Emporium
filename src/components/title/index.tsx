f912fa07102ef7c711cde45ef0ceb3422ff82923
Thayane Bomfim <thayanesilbomfim@outlook.com>
2023-06-26 21:37:17 -0300

    create article page

--- src/components/title/index.tsx
+++ src/components/title/index.tsx
@@ -3,13 +3,21 @@ import { Row } from "react-bootstrap";
 import 'react-quill/dist/quill.snow.css';
 import './styles.scss'
 
-export default function Title({ title }: any) {
-
+export default function Title({ title, subtitle, isCentered }: any) {
 
     return (
-        <Row className="my-5">
-            <h2 className="title">{title}</h2>
-        </Row>
+        <>
+
+            {
+                isCentered ? <Row className="my-5 text-center titleCentered">
+                    <h2>{title}</h2>
+                    <h3>{subtitle}</h3>
+                </Row> : <Row className="my-5">
+                    <h2 className="title">{title}</h2>
+                </Row>
+            }
+
+        </>
 
     )
 
--- src/pages/article/index.tsx
+++ src/pages/article/index.tsx
@@ -1,5 +1,12 @@
+import { Container } from "react-bootstrap";
+import Title from "../../components/title";
+
 export function Article() {
     return (
-        <p>Batata</p>
+        <Container>
+
+            <Title title="Conteúdos indisponíveis" subtitle="Testando subtitulo" isCentered={true} />
+
+        </Container>
     )
 }
